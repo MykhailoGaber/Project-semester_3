3bcf0e525b55ee1f534492cf454b8d5ad54bbd9b
Roman Khodosov <roma.pro.khodosov@gmail.com>
2026-05-10 18:39:31 +0300

    додав основні фвйли та гілку render_game

---
+++ src/js/data/games/test_game.js
@@ -0,0 +1,5 @@
+// export const initTestGame = () => {
+//   const gamesContainer = document.querySelector('[data-games]');
+//   const gameHtml = '<h2>test game<h2>';
+//   gamesContainer.innerHTML = gameHtml;
+// };
