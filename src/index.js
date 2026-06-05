6aae5e5aeda126cf3bc2e77f715eb5734bf88822
Roman Khodosov <roma.pro.khodosov@gmail.com>
2026-06-05 11:55:52 +0300

    виправив баги та додав адаптив

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 import './js/main_content.js';
-// import './js/modalwindow_footer.js';
-// import './js/modalwindow_header.js';
+import './js/modalwindow_footer.js';
+import './js/modalwindow_header.js';
